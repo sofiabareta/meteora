52ba3c203710461ab9a5740093b9cf3588db08a7
Sofia <sofibareta@gmail.com>
2023-06-28 15:40:17 -0300

    Fix footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -5,7 +5,7 @@ import Legal from "./Legal"
 const Footer = () => {
     return(
         <div className="bg-black p-4">
-            <Legal>2023 © Desenvolvido por Alura | <br /> Projeto fictício sem fins comerciais.</Legal>
+            <Legal>{`2023 © Desenvolvido por Alura |${<br />} Projeto fictício sem fins comerciais.`}</Legal>
         </div>
     )
 }
